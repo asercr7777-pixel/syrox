7fe543202a41dfeb7d6458857be170129978dd3d
asercr7777-pixel <asercr7777@gmail.com>
2026-08-19 17:49:22 +0300

    feat: extend story index to 30 Arc 1 chapters

--- src/data/story/index.ts
+++ src/data/story/index.ts
@@ -1,3 +1,34 @@
 export * from './types';
 export * from './npcs';
-export * from './chapters';
+export { STORY_CHAPTERS, STORY_CHAPTERS_EXPANSION } from './chapters';
+export { ARC1_CHAPTERS_16_30 } from './arc1Chapters16to30';
+
+import type { StoryChapter } from './types';
+import { STORY_CHAPTERS, STORY_CHAPTERS_EXPANSION } from './chapters';
+import { ARC1_CHAPTERS_16_30 } from './arc1Chapters16to30';
+
+export const ALL_CHAPTERS: StoryChapter[] = [
+  ...STORY_CHAPTERS,
+  ...STORY_CHAPTERS_EXPANSION,
+  ...ARC1_CHAPTERS_16_30,
+];
+
+export function getChapterById(id: string): StoryChapter | undefined {
+  return ALL_CHAPTERS.find((chapter) => chapter.id === id);
+}
+
+export function getChapterByNumber(number: number): StoryChapter | undefined {
+  return ALL_CHAPTERS.find((chapter) => chapter.number === number);
+}
+
+export function getTotalChapters(): number {
+  return ALL_CHAPTERS.length;
+}
+
+export function getMissionById(id: string): import('./types').StoryMission | undefined {
+  for (const chapter of ALL_CHAPTERS) {
+    const mission = chapter.missions.find((item) => item.id === id);
+    if (mission) return mission;
+  }
+  return undefined;
+}

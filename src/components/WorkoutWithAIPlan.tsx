e1599981f538479e28608e326fad7e751cb9d33f
asercr7777-pixel <asercr7777@gmail.com>
2026-09-16 01:25:38 +0300

    fix: route workout page to six-day system

--- src/components/WorkoutWithAIPlan.tsx
+++ src/components/WorkoutWithAIPlan.tsx
@@ -1,13 +1,6 @@
-import { Workout2 } from './Workout2';
-import { WorkoutDayPlanBar } from './WorkoutDayPlanBar';
+import { SixDayWorkout } from './SixDayWorkout';
 import './workout-spacing.css';
-import './workout-six-day.css';
 
 export function WorkoutWithAIPlan() {
-  return (
-    <>
-      <WorkoutDayPlanBar />
-      <Workout2 />
-    </>
-  );
+  return <SixDayWorkout />;
 }

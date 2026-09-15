f6a876f4a6d96a540a290b7de3309dffffb48e3d
asercr7777-pixel <asercr7777@gmail.com>
2026-09-16 01:21:21 +0300

    feat: switch workout navigation to six customizable days

--- src/components/WorkoutWithAIPlan.tsx
+++ src/components/WorkoutWithAIPlan.tsx
@@ -1,6 +1,13 @@
 import { Workout2 } from './Workout2';
+import { WorkoutDayPlanBar } from './WorkoutDayPlanBar';
 import './workout-spacing.css';
+import './workout-six-day.css';
 
 export function WorkoutWithAIPlan() {
-  return <Workout2 />;
+  return (
+    <>
+      <WorkoutDayPlanBar />
+      <Workout2 />
+    </>
+  );
 }
